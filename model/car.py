2ca77d58596465a5fcc2c67fbb10ed689aabfbb9
HRZ1997 <92747163+HRZ1997@users.noreply.github.com>
2022-04-29 20:08:47 -0400

    Update car.py

--- model/car.py
+++ model/car.py
@@ -1,12 +1,13 @@
 from model.serviceable import Serviceable
 
 class Car(Serviceable):
-    def __init__(self, Engine, Battery):
+    def __init__(self, Engine, Battery, Tires):
         self.engine = Engine
         self.battery = Battery
+        self.tires = Tires
 
     def needs_service(self):
-        if self.battery.battery_needs_service() or self.engine.engine_needs_service():
+        if self.battery.battery_needs_service() or self.engine.engine_needs_service() or self.tires.tires_need_service():
             return True
         else:
             return False
