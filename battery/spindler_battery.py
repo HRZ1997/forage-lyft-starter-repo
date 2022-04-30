6800d6af7d9984277cce8347e24604825b38c25d
HRZ1997 <92747163+HRZ1997@users.noreply.github.com>
2022-04-29 20:01:31 -0400

    Update spindler_battery.py

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -6,5 +6,5 @@ class SpindlerBattery(Battery):
         self.current_date = current_date
 
     def battery_needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 3)
         return service_threshold_date < self.current_date
